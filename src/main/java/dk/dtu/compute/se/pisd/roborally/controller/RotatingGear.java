8ddd11a283ae5e800b6b4cc4cf839ee79ef0d1fa
mikael <mikael.fangel@pm.me>
2022-04-21 16:14:05 +0200

    added rotating gear method and skeletons for other space controllers

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/RotatingGear.java
@@ -0,0 +1,34 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+public class RotatingGear extends FieldAction{
+    private enum Direction{
+        LEFT,
+        RIGHT;
+    }
+
+    private Direction direction;
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    @Override
+    public boolean doAction(GameController gameController, Space space) {
+        if(space.getActions().size() > 0) {
+            RotatingGear gear = (RotatingGear) space.getActions().get(0);
+            if(gear.getDirection() == Direction.LEFT) {
+                gameController.rmc.turnLeft(gameController.board.getCurrentPlayer());
+            } else {
+                gameController.rmc.turnRight(gameController.board.getCurrentPlayer());
+            }
+        }
+        
+        return true;
+    }
+}
