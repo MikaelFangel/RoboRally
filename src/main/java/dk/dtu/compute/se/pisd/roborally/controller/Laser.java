a8b07578c9ec231efc740833e973239f481a6d2e
mikael <mikael.fangel@pm.me>
2022-04-23 02:03:33 +0200

    initial commit for lasers

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Laser.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Laser.java
@@ -1,10 +1,48 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
-public class Laser extends FieldAction{
+public class Laser extends FieldAction {
+
+    private int numberOfLasers;
+    private Heading heading;
+
+    public Heading getHeading() {
+        return heading;
+    }
+
+    public void setHeading(Heading heading) {
+        this.heading = heading;
+    }
+
+    public void setNumberOfLasers(int numberOfLasers) {
+        this.numberOfLasers = numberOfLasers;
+    }
+
+    public int getNumberOfLasers() {
+        return numberOfLasers;
+    }
+
+    // TODO test function
     @Override
     public boolean doAction(GameController gameController, Space space) {
+        if (space.getActions().size() > 0) {
+            Laser laser = (Laser) space.getActions().get(0);
+            Heading direction = laser.getHeading();
+
+            Space next = gameController.board.getNeighbour(space, direction);
+
+            while (next != null && next.getPlayer() == null) {
+                next = gameController.board.getNeighbour(next, direction);
+            }
+
+            if (next.getPlayer() != null) {
+                // Give player damage card
+            }
+        } else {
+            return false;
+        }
         return false;
     }
 }
