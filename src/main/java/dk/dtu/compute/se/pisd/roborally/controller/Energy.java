10c4bce2a520a080dd764c9e21ccd78ecdd9cc0f
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-05-04 22:54:53 +0200

    corrected sted to 4 and not 5 due to zero indexing

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Energy.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Energy.java
@@ -15,7 +15,7 @@ public class Energy extends FieldAction {
         boolean result = false;
         if (space.getActions().size() > 0) {
             Energy energy = (Energy) space.getActions().get(0);
-            if (energy.hasEnergyCube || gameController.board.step == 5) {
+            if (energy.hasEnergyCube || gameController.board.step == 4) {
                 space.getPlayer().energyCount++;
                 energy.hasEnergyCube = false;
                 result = true;
