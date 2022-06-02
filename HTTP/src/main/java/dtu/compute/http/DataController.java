9b44386a0dc881b3edfa88596733331e3f75fbf1
Christian Andersen <s133288@dtu.dk>
2022-06-02 13:52:25 +0200

    Server side Rest API

---
+++ HTTP/src/main/java/dtu/compute/http/DataController.java
@@ -0,0 +1,26 @@
+package dtu.compute.http;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class DataController {
+
+    @Autowired
+    private IStatusComm statusComm;
+
+    @GetMapping(value = "/gameState")
+    public ResponseEntity<String> getGameState()
+    {
+        return ResponseEntity.ok().body(statusComm.getGameState());
+    }
+
+    @PutMapping(value = "/gameState")
+    public ResponseEntity<String> setGameState(@RequestBody String g)
+    {
+        statusComm.updateGame(g);
+        return ResponseEntity.ok().body("OK");
+    }
+}
