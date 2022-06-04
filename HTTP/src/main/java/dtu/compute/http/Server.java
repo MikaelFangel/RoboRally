0597d07e8d0179472b9b85f688de251500806b34
Christian Andersen <s133288@dtu.dk>
2022-06-04 13:45:15 +0200

    rework on server side database

---
+++ HTTP/src/main/java/dtu/compute/http/Server.java
@@ -0,0 +1,28 @@
+package dtu.compute.http;
+
+import org.springframework.stereotype.Service;
+
+public class Server {
+    private String id;
+    private String title;
+    private transient String gameState;
+    private int amountOfPlayers;
+
+    public Server(String title, int id){
+        this.id = String.valueOf(id);
+        this.title = title;
+        this.amountOfPlayers = 0;
+    }
+
+    public void addPlayer(){
+        amountOfPlayers++;
+    }
+
+    public String getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(String gameState) {
+        this.gameState = gameState;
+    }
+}
---
+++ HTTP/src/main/java/dtu/compute/http/ServerController.java
@@ -0,0 +1,49 @@
+package dtu.compute.http;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ServerController {
+
+    @Autowired
+    private IStatusComm statusComm;
+
+    //host Game
+    @PostMapping(value = "/game")
+    public ResponseEntity<String> createGame(@RequestBody String s){
+        String newServerID = statusComm.hostGame(s);
+        return ResponseEntity.ok().body(newServerID);
+    }
+
+    //listOfGame
+    @GetMapping(value = "/game")
+    public ResponseEntity<String> listOfGame(){
+        String servers = statusComm.listGames();
+        return ResponseEntity.ok().body(servers);
+    }
+
+    //Join game
+    @PutMapping(value = "/game/{id}")
+    public ResponseEntity<String> joinGame(@PathVariable String id){
+        statusComm.joinGame(id);
+        return ResponseEntity.ok().body("ok");
+    }
+
+    @GetMapping(value = "/gameState/{id}")
+    public ResponseEntity<String> getGameState(@PathVariable String id)
+    {
+        return ResponseEntity.ok().body(statusComm.getGameState(id));
+    }
+
+    @PutMapping(value = "/gameState/{id}")
+    public ResponseEntity<String> setGameState(@PathVariable String id, @RequestBody String g)
+    {
+        statusComm.updateGame(id,g);
+        return ResponseEntity.ok().body("ok");
+    }
+}
