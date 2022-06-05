f027dbd481144e4c153bbff1f5b88f1b63dea975
Frederik Greve Petersen <frederik.greve@gmail.com>
2022-06-05 16:01:27 +0200

    Made test to serializing game and fixed not setting currentPlayer on templates

---
+++ Game/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/SerializeTest.java
@@ -0,0 +1,31 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import dk.dtu.compute.se.pisd.roborally.fileaccess.SerializeState;
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import org.junit.jupiter.api.*;
+
+public class SerializeTest {
+
+    @Test
+    void repeatedSerializesAndDeserializesOfDefaultBoard(){
+        Board startBoard = SaveLoadGame.newBoard(2, "dizzy_highway");
+        String jsonResult1 = SerializeState.serializeGame(startBoard);
+
+        Board board1 = SerializeState.deserializeGame(jsonResult1, false);
+        String jsonResult2 = SerializeState.serializeGame(board1);
+
+        Assertions.assertEquals(jsonResult1, jsonResult2);
+    }
+
+    @Test
+    void repeatedSerializesAndDeserializesOfSavedBoard(){
+        Board startBoard = SaveLoadGame.loadBoard("Test204");
+        String jsonResult1 = SerializeState.serializeGame(startBoard);
+
+        Board board1 = SerializeState.deserializeGame(jsonResult1, true);
+        String jsonResult2 = SerializeState.serializeGame(board1);
+
+        Assertions.assertEquals(jsonResult1, jsonResult2);
+    }
+
+}
