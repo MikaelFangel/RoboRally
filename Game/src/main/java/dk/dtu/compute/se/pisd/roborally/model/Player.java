5ea7c0fa4f66e863a30e5323d8212d670df5f28f
Frederik Greve Petersen <frederik.greve@gmail.com>
2022-06-08 10:23:27 +0200

    Made priority variable for player

--- Game/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
+++ Game/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
@@ -49,6 +49,7 @@ public class Player extends Subject {
     public String color;
     public int energyCount;
     public int checkPoints;
+    public int priority;
 
     public Space space;
     public Heading heading = SOUTH;
@@ -96,6 +97,14 @@ public class Player extends Subject {
                 space.playerChanged();
             }
         }
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
     }
 
     public String getColor() {
