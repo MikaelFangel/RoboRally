5313ad96a0e4c47412edbfda6a6247889b82dddd
Frederik Greve Petersen <frederik.greve@gmail.com>
2022-06-10 14:45:32 +0200

    Players move from reboot token if another respawns

--- Game/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/fieldaction/Pit.java
+++ Game/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/fieldaction/Pit.java
@@ -1,24 +1,17 @@
 package dk.dtu.compute.se.pisd.roborally.controller.fieldaction;
 
 import dk.dtu.compute.se.pisd.roborally.controller.GameController;
-import dk.dtu.compute.se.pisd.roborally.controller.SaveLoadGame;
-import dk.dtu.compute.se.pisd.roborally.controller.fieldaction.FieldAction;
-import dk.dtu.compute.se.pisd.roborally.fileaccess.ReadWriteGame;
-import dk.dtu.compute.se.pisd.roborally.model.Board;
-import dk.dtu.compute.se.pisd.roborally.model.Command;
-import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
-import java.util.Objects;
 
 /**
- * @author Ahmad Sandhu, Mikael Fangel
+ * @author Ahmad Sandhu, Frederik Petersen
  */
 public class Pit extends FieldAction {
     @Override
     public boolean doAction(GameController gameController, Space space) {
         // Find reboot token.
-        RebootToken.respawnPlayer(gameController.board, space.getPlayer());
+        RebootToken.respawnPlayer(gameController, space.getPlayer());
 
         return true;
     }
